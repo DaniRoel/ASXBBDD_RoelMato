50e2b41c469efff8e1e3519372d85d0f6577aa60
RoelMato <atlasmongodb.daniroel@gmail.com>
2025-12-27 14:03:30 +0100

    Actualizar_PowerShell

--- PowerShell.ps1
+++ PowerShell.ps1
@@ -26,4 +26,36 @@ Install-Module -Name SqlServer -AllowClobber
 Get-Module SqlServer -ListAvailable
 #Comprobar comandos
 Get-Command -Module SqlServer -CommandType Cmdlet | Out-GridView 
-Get-Command -Module SqlServer | Out-GridView 
+Get-Command -Module SqlServer | Out-GridView
+#Comprobar Servicios
+Get-Service | where-Object{$_.name -like '*sql*'} | Format-Table –AutoSize
+#Arrancar Servicio
+Start-Service "SQLSERVERAGENT"
+Get-Service | ?{$_.name -like '*SQLSERVERAGENT*'} | ogv
+Stop-Service "SQLSERVERAGENT"
+Get-Service | ?{$_.name -like '*SQLSERVERAGENT*'} | ogv
+#Invoke-Sqlcmd
+Invoke-Sqlcmd -Query "SELECT * FROM PRODUCTO" -ConnectionString "Data Source=.;Initial Catalog=Herboristeria;Integrated Security=True;TrustServerCertificate=True;ApplicationIntent=ReadOnly" | Format-Table -AutoSize
+Invoke-Sqlcmd -ServerInstance "localhost" -Database "Herboristeria" -Username "sa" -Password "sa" -Query "SELECT * FROM VENTA" -TrustServerCertificate
+#Backups
+Backup-SqlDatabase -ServerInstance "localhost" -Database "Herboristeria"
+#Backup_ruta
+$dt = Get-Date -Format dd-MMM-yy
+$instancename = "localhost"
+$dbname = 'Herboristeria'
+Backup-SqlDatabase -Serverinstance $instancename -Database $dbname -BackupFile "C:\Backup\BK_$($dbname)_$($dt).bak"
+#Backup Invoke
+Invoke-Sqlcmd -Query "BACKUP DATABASE Herboristeria TO DISK = 'C:\Backup\Invoke_herboristeria.bak' WITH FORMAT" -ConnectionString "Server=localhost;Integrated Security=True;TrustServerCertificate=True"
+
+#Restore
+    #Eliminar BBDD
+$instancia = "localhost"
+$bbdd = 'Herboristeria'
+Invoke-Sqlcmd -Serverinstance $instancia -TrustServerCertificate -Query "Drop database $bbdd;"
+    #Recuperar BBDD
+$instancia = "localhost"
+$bbdd = 'Herboristeria'
+$archivo = 'C:\Backup\BK_Herboristeria_27-dic.-25.bak'
+Restore-Sqldatabase -Serverinstance $instancia -Database $bbdd -Backupfile $archivo -replacedatabase
+#Comprobar
+Invoke-Sqlcmd -ServerInstance $instancia -Database $bbdd -Username "sa" -Password "sa" -Query "SELECT * FROM PRODUCTO" -TrustServerCertificate
