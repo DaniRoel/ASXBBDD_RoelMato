bb3e34c2c991872b56ca52745f8163c4f54a275d
RoelMato <atlasmongodb.daniroel@gmail.com>
2025-12-27 15:29:22 +0100

    dbatools

--- PowerShell.ps1
+++ PowerShell.ps1
@@ -59,3 +59,43 @@ $archivo = 'C:\Backup\BK_Herboristeria_27-dic.-25.bak'
 Restore-Sqldatabase -Serverinstance $instancia -Database $bbdd -Backupfile $archivo -replacedatabase
 #Comprobar
 Invoke-Sqlcmd -ServerInstance $instancia -Database $bbdd -Username "sa" -Password "sa" -Query "SELECT * FROM PRODUCTO" -TrustServerCertificate
+
+
+#DBA Tools
+#Instalar
+Install-Module -Name dbatools 
+#Actualizar DBATOOLS
+Update-Module -Name dbatools 
+#Test DBA
+Get-Module dbatools -ListAvailable
+Test-DbaConnection localhost 
+Get-DbaDatabase -SqlInstance "Data Source=localhost;TrustServerCertificate=True;" -Database herboristeria 
+#Crear BBDD
+$instancia =  "Data Source=localhost;TrustServerCertificate=True;"
+$Nombre = 'HerboristeriaDBA'
+$Recoverymodel = 'Simple'
+$Owner = 'sa'
+$PrimaryFilesize = 1024
+$PrimaryFileGrowth = 256
+$LogSize = 512
+$LogGrowth = 128
+New-DbaDatabase -SqlInstance $instancia -Name $Nombre -Recoverymodel $Recoverymodel -Owner $Owner
+#Comprobar
+Get-DbaDatabase -SqlInstance $instancia -Database $Nombre
+#Backup
+    #Creo Carpeta
+New-Item "C:\Backup\dbatools" -ItemType Directory
+    #Lanzo Backup
+$destino = 'C:\Backup\dbatools\'
+Backup-DbaDatabase -SqlInstance  $instancia -Path $destino -Database $Nombre -Type Full
+cd C:\Backup\dbatools
+ls
+
+#Borrar BBDD
+Get-DbaDatabase -SqlInstance $instancia -Database $Nombre
+Remove-DbaDatabase -SqlInstance $instancia -Database $nombre
+Get-DbaDatabase -SqlInstance $instancia -Database $Nombre
+#Restaurar DBA
+Restore-DbaDatabase -SqlInstance $instancia -Path $destino -DestinationDataDirectory 'C:\BD Ejemplo' -WithReplace
+Restore-DbaDatabase -SqlInstance $instancia -Path $destino
+Get-DbaDatabase -SqlInstance $instancia -Database $Nombre
