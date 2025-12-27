fc40222188f53ba26bf53dc3e284d4cedccf8b61
RoelMato <atlasmongodb.daniroel@gmail.com>
2025-12-27 12:23:52 +0100

    Powershell

---
+++ PowerShell.ps1
@@ -0,0 +1,29 @@
+﻿#Ayuda
+Get-Help
+Get-Help *SQL*
+Get-Help -showWindow
+
+#Variables
+$nombre=hostname
+$nombre
+
+#Restriccion
+Get-ExecutionPolicy
+Set-ExecutionPolicy Unrestricted
+Get-ExecutionPolicy
+
+#Alias
+Get-Alias
+$nombre | out-file C:\hostname.txt
+Get-Alias
+New-Alias -Name notas -Value Notepad
+notas
+
+
+#Instalar SQL
+Install-Module -Name SqlServer -AllowClobber
+#Comprobar instalación
+Get-Module SqlServer -ListAvailable
+#Comprobar comandos
+Get-Command -Module SqlServer -CommandType Cmdlet | Out-GridView 
+Get-Command -Module SqlServer | Out-GridView 
